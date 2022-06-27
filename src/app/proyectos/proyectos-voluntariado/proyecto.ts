aad57efe482be14f68bd32d7931ea2539ebcfee7
gabriela-oss <solisch.gabriela@gmail.com>
2022-06-27 11:23:59 -0600

    mostrar la info con el get de proyectos-voluntariado y diseño del form

--- src/app/proyectos/proyectos-voluntariado/proyecto.ts
+++ src/app/proyectos/proyectos-voluntariado/proyecto.ts
@@ -1,4 +1,3 @@
-
 export interface Proyecto{
     name: string;
     country: string;
--- src/app/proyectos/proyectos-voluntariado/proyectos-voluntariado.component.ts
+++ src/app/proyectos/proyectos-voluntariado/proyectos-voluntariado.component.ts
@@ -12,43 +12,15 @@ import { Proyecto } from './proyecto';
 })
 export class ProyectosVoluntariadoComponent implements OnInit {
 
-  proyectos:Proyecto[]=[]
-
-  // voluntarios:Voluntario[] = [
-  //   new Voluntario(1, "Carlos Solis Chavarria", 47, "Costa Rica"),
-  //   new Voluntario(2, "William Solis Chavarria", 45, "Costa Rica"),
-  //   new Voluntario(3, "Joaquin Solis Chavarria", 44, "Costa Rica"),
-  //   new Voluntario(4, "Diego Solis Chavarria", 40, "Costa Rica"),
-  //   new Voluntario(5, "Steven Solis Chavarria", 23, "Costa Rica")
-  // ];
-  
-  // organizaciones:Organizacion[] = [
-  //   new Organizacion(1, "Fusavo", "Costa Rica"),
-  //   new Organizacion(2, "Consejo de Europa", "Europa"),
-  //   new Organizacion(3, "Banco Mundial", "Estados Unidos"),
-  //   new Organizacion(4, "Lucema", "Costa Rica"),
-  //   new Organizacion(5, "PRO", "Costa Rica")
-  // ];
-
-  // proyectos:ProyectoVoluntariado[]= [
-  //   new ProyectoVoluntariado("Salvando Alcoholicos", "Costa Rica", "30", "5 semanas", this.voluntarios[0], this.organizaciones[0]),
-  //   new ProyectoVoluntariado("Consejos", "Costa Rica", "300", "4 semanas", this.voluntarios[1], this.organizaciones[1]),
-  //   new ProyectoVoluntariado("Administrando tu dinero", "Costa Rica", "500", "3 semanas", this.voluntarios[2], this.organizaciones[2]),
-  //   new ProyectoVoluntariado("Richy", "Costa Rica", "100", "2 semanas", this.voluntarios[3], this.organizaciones[3]),
-  //   new ProyectoVoluntariado("Ayudando a Drogadictos", "Costa Rica", "50", "6 semanas", this.voluntarios[4], this.organizaciones[4])
-  // ];
-
+  proyectos:any;
   constructor(/*ruta:ActivatedRoute*/ private http:HttpClient) {
     
    }
    
    ngOnInit(): void {
-  }
-  
-    getData(){
-    this.http.get<any>('https://tutoriadw.ronnysantamaria.repl.co/api/get').subscribe(data=>{
-      this.proyectos = data;
-      console.log(this.proyectos);
-    });
+     this.http.get<any>('https://tutoriadw.ronnysantamaria.repl.co/api/get').subscribe((data)=>{
+       this.proyectos = data;
+       console.log(this.proyectos);
+     });
   }
 }
